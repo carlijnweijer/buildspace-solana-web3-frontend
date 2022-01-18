2a14328b3d879cbf9503b5f5fe80578cc92500a6
carlijnweijer <carlijnweijer@gmail.com>
2022-01-18 16:23:17 +0100

    ✨ inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-7xl">
+        <span className="text-8xl">Share</span> your{" "}
+        <span className="font-semibold">Web3</span>{" "}
+        <span className="">goals</span> for 2022
+      </h1>
+    </div>
+  );
+}
+
+export default App;
